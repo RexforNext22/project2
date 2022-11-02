0e8623cdb0b97b8f15b5260d9bc9b49a028b86aa
mr_ryan <61605919+RexforNext22@users.noreply.github.com>
2022-11-02 15:33:35 -0600

    Added by Ryan

---
+++ part2.ps1
@@ -0,0 +1,13 @@
+# This is script will output information about the current machine.
+# Authors: Ryan Pinkney, Tanner Davis, Tyler Steele, Kevin Gutierrez
+
+# Parameters
+
+# Functions
+
+# Main
+
+
+
+Write-Host "Hey There"
+
