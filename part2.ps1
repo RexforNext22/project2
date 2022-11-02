d9fd76081027fe1e9c9de54fa6e06f91ae2f8454
mr_ryan <61605919+RexforNext22@users.noreply.github.com>
2022-11-02 15:58:35 -0600

    Added by Ryan

--- part2.ps1
+++ part2.ps1
@@ -2,12 +2,60 @@
 # Authors: Ryan Pinkney, Tanner Davis, Tyler Steele, Kevin Gutierrez
 
 # Parameters
+param(
+     [Parameter()]
+     [string]$inputip
+ )
 
 # Functions
 
+function Return-Bios([string]$ip, [switch]$remote){
+
+    if($remote) {
+        Get-WmiObject win32_bios -ComputerName $ip
+    } else {
+        Get-WmiObject win32_bios -ComputerName .
+    }
+    
+}
+
+function Return-Process([string]$ip, [switch]$remote){
+
+    if($remote) {
+        Get-WmiObject win32_process -ComputerName $ip
+    } else {
+        Get-WmiObject win32_process -ComputerName . |  Select -First 5 | Format-Table
+    }
+    
+}
+
+function Return-Product([string]$ip, [switch]$remote){
+
+    if($remote) {
+        Get-WmiObject win32_product -ComputerName $ip
+    } else {
+        Get-WmiObject win32_product -ComputerName . |  Select -First 5 | Format-Table
+    }
+    
+}
+
 # Main
+Write-Host "--------------------------------------------------------"
+Write-Host "BIOS INFORMATION"
+Return-Bios 
+Write-Host "--------------------------------------------------------"
+Write-Host "PROCESS INFORMATION"
+Return-Process 
+Write-Host "--------------------------------------------------------"
+Write-Host "SOFTWARE INFORMATION"
+Return-Poduct
+Write-Host "--------------------------------------------------------"
 
 
 
-Write-Host "Hey There"
+
+
+
+
+
 
